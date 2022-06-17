3bc46e64629a95a1c5faf339cf4b1b14e6c26874
Khar Dmitry <qwedsazxc8250@gmail.com>
2022-06-17 17:00:03 +0300

    1

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,46 +1,44 @@
-package com.tkroman.kpi.y2022.l1
+//Я вважаю, що у моїй реалізації не можна було створити порожнє дерево, тому я додав case Empty, додав тести, та прибрав
+// сміття
 
+
+package com.tkroman.kpi.y2022.l1
+import scala.annotation.tailrec
 
 enum Tree[+A]:
   case Branch(l: Tree[A], r: Tree[A], v: A)
   case Leaf(a: A)
+  case Empty
 
 
 def size[A](t: Tree[A]): Int = {
   t match{
     case Tree.Branch(l, r, v) => 1 + size(l) + size(r)
     case Tree.Leaf(a) => 1
+    case Tree.Empty => 0
   }
 }
 def depth[A](t: Tree[A]): Int = {
   t match{
     case Tree.Branch(l, r, v) => Math.max(depth(l), depth(r)) + 1
     case Tree.Leaf(a) => 0
+    case Tree.Empty => 0
   }
 }
 def map[A, B](t: Tree[A], f: A => B): Tree[B] = {
   t match{
     case Tree.Branch(l, r, v) => Tree.Branch(map(l, f), map(r, f), f(v))
     case Tree.Leaf(a) => Tree.Leaf(f(a))
+    case Tree.Empty => Tree.Empty
   }
 }
-def fold[A](t: Tree[A], f:(A, A) => A): A = {
+def fold[A](t: Tree[A], z: A, f:(A, A) => A): A = {            //z-нейтральний елемент в системі з операцією f
   t match{
-    case Tree.Branch(l, r, v) => f(f(fold(l, f), fold(r, f)), v)
+    case Tree.Branch(l, r, v) => f(f(fold(l, z, f), fold(r, z, f)), v)
     case Tree.Leaf(a) => a
+    case Tree.Empty => z
   }
 }
 
 @main def run() =
-  val tree = Tree.Leaf(2)
-  val tree1 = Tree.Leaf(3)
-  val tree11 = Tree.Leaf(7)
-  val tree2 = Tree.Branch(tree, tree1, 4)
-  val tree3 = Tree.Branch(tree2, tree11, 5)
-  val tree4 = Tree.Leaf(1)
-  val tree5 = Tree.Leaf(6)
-  val tree6 = Tree.Leaf(8)
-  val tree7 = Tree.Branch(tree4, tree5, 9)
-  val tree8 = Tree.Branch(tree7, tree6, 10)
-  val tree9 = Tree.Branch(tree3, tree8, 11)
-  println(tree3.getClass)
+  println("Hello")
--- src/test/scala/TreeSuite.scala
+++ src/test/scala/TreeSuite.scala
@@ -2,24 +2,84 @@ package com.tkroman.kpi.y2022.l1
 import munit.FunSuite
 
 class TreeSuite extends FunSuite {
-  test("size") {
+  test("size_test1") {
     val expected = 3
     val actual = size(Tree.Branch(Tree.Leaf(2), Tree.Leaf(3), 4))
     assertEquals(actual, expected)
   }
-  test("depth") {
+  test("size_test2") {
+    val expected = 0
+    val actual = size(Tree.Empty)
+    assertEquals(actual, expected)
+  }
+  test("size_test3") {
+    val expected = 2
+    val actual = size(Tree.Branch(Tree.Empty, Tree.Leaf(3), 4))
+    assertEquals(actual, expected)
+  }
+  test("size_test4") {
+    val expected = 3
+    val actual = size(Tree.Branch(Tree.Branch(Tree.Leaf(5), Tree.Empty, 6), Tree.Empty, "Hello"))
+    assertEquals(actual, expected)
+  }
+  test("depth_test1") {
     val expected = 2
     val actual = depth(Tree.Branch(Tree.Branch(Tree.Leaf(2), Tree.Leaf(3), 4), Tree.Leaf(5), 6))
     assertEquals(actual, expected)
   }
-  test("map") {
+  test("depth_test2") {
+    val expected = 0
+    val actual = depth(Tree.Empty)
+    assertEquals(actual, expected)
+  }
+  test("depth_test3") {
+    val expected = 2
+    val actual = depth(Tree.Branch(Tree.Branch(Tree.Leaf("2"), Tree.Empty, "4"), Tree.Leaf("5"), "6"))
+    assertEquals(actual, expected)
+  }
+  test("depth_test4") {
+    val expected = 2
+    val actual = depth(Tree.Branch(Tree.Branch(Tree.Leaf(9), Tree.Empty, 4), Tree.Empty, 6))
+    assertEquals(actual, expected)
+  }
+  test("map_test1") {
     val expected = Tree.Branch(Tree.Branch(Tree.Leaf("2Hello"), Tree.Leaf("3Hello"), "4Hello"), Tree.Leaf("5Hello"), "6Hello")
     val actual = map(Tree.Branch(Tree.Branch(Tree.Leaf("2"), Tree.Leaf("3"), "4"), Tree.Leaf("5"), "6"), (a) => a+"Hello")
     assertEquals(actual, expected)
   }
-  test("fold") {
+  test("map_test2") {
+    val expected = Tree.Empty
+    val actual = map(Tree.Empty, (a) => "Hello")
+    assertEquals(actual, expected)
+  }
+  test("map_test3") {
+    val expected = Tree.Branch(Tree.Branch(Tree.Leaf("2Hello"), Tree.Empty, "4Hello"), Tree.Leaf("5Hello"), "6Hello")
+    val actual = map(Tree.Branch(Tree.Branch(Tree.Leaf("2"), Tree.Empty, "4"), Tree.Leaf("5"), "6"), (a) => a+"Hello")
+    assertEquals(actual, expected)
+  }
+  test("map_test4") {
+    val expected = Tree.Branch(Tree.Branch(Tree.Leaf(6), Tree.Empty, 12), Tree.Empty, 18)
+    val actual = map(Tree.Branch(Tree.Branch(Tree.Leaf(2), Tree.Empty, 4), Tree.Empty, 6), (a) => a*3)
+    assertEquals(actual, expected)
+  }
+  test("fold_test1") {
     val expected = 720
-    val actual = fold(Tree.Branch(Tree.Branch(Tree.Leaf(2), Tree.Leaf(3), 4), Tree.Leaf(5), 6), (a, b) => a*b)
+    val actual = fold(Tree.Branch(Tree.Branch(Tree.Leaf(2), Tree.Leaf(3), 4), Tree.Leaf(5), 6), 1, (a, b) => a*b)
+    assertEquals(actual, expected)
+  }
+  test("fold_test2") {
+    val expected = 17
+    val actual = fold(Tree.Branch(Tree.Branch(Tree.Leaf(2), Tree.Empty, 4), Tree.Leaf(5), 6), 0, (a, b) => a+b)
+    assertEquals(actual, expected)
+  }
+  test("fold_test3") {
+    val expected = 1
+    val actual = fold(Tree.Empty, 1, (a, b) => a*b)
+    assertEquals(actual, expected)
+  }
+  test("fold_test4") {
+    val expected = "2456"
+    val actual = fold(Tree.Branch(Tree.Branch(Tree.Leaf("2"), Tree.Empty, "4"), Tree.Leaf("5"), "6"), "", (a, b) => a + b)
     assertEquals(actual, expected)
   }
 }
